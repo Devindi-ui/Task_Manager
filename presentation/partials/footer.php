cd9d62beecdce39c4c17343a3cf30754a26c5eaa
Devindi-u <panchalidevindi@gmail.com>
2025-07-23 12:21:09 +0530

    footer

---
+++ presentation/partials/footer.php
@@ -0,0 +1,8 @@
+<footer class="mt-5 text-center text-muted">
+    <div class="container">
+        <p class="mb-0">
+            Task Manager developed at BMI Campus &copy; </= date("Y") ?>
+            <a href="https://github.com/Devindi-ui">Devindi</a>
+        </p>
+    </div>
+</footer>
